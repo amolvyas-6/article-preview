58d475711ee2a0edb36277016ccfc2e7f87c62e2
Amol Vyas <amol.vyas0630@gmail.com>
2025-03-11 12:35:02 +0530

    made website responsive

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const shareBtn = document.querySelector('#shareBtn');
 const shareDivDesktop = document.querySelector('.share-div-desktop');
+const shareBtnMobile = document.querySelector('#shareBtnMobile');
+const shareDivMobile = document.querySelector('.share-div-mobile');
 
 shareBtn.addEventListener('click', function(){
     if(window.innerWidth > 784){
         shareBtn.classList.toggle('btn-light');
         shareBtn.classList.toggle('btn-dark');
         shareDivDesktop.classList.toggle('hidden');
+    }
+    // console.log("hello");
+    else {
+        shareDivMobile.classList.toggle('hidden');
+    }
+})
+
+shareBtnMobile.addEventListener('click', function(){
+    shareDivMobile.classList.toggle('hidden');
+})
+
+
+window.addEventListener('resize', function(){
+    if (window.innerWidth > 784){
+        shareDivMobile.classList.add('hidden');
+    }
+    else{
+        shareDivDesktop.classList.add('hidden');
+        shareBtn.classList.remove('btn-dark');
+        shareBtn.classList.add('btn-light')
     }
 })
